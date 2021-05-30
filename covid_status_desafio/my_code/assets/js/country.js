50820faca92443eb9561f9486701e7cf98e45a0e
Boanerges Junior <boanerges_junior@hotmail.com>
2021-05-30 18:45:44 -0300

    finished daily curve chart

--- covid_status_desafio/my_code/assets/js/country.js
+++ covid_status_desafio/my_code/assets/js/country.js
@@ -20,52 +20,78 @@ async function handlerFilter(){
     let country = document.getElementById('cmbCountry')
     let selectFilterValue = document.getElementById('cmbData')
 
+    let firstDateBeforeDateStart = getFirstDate(dateStart.value)
+
     let [summary, filter] = await Promise.allSettled([
         fetch('https://api.covid19api.com/summary'),
-        fetch(`https://api.covid19api.com/country/${country.value}?from=${dateStart.value}T00:00:00Z&to=${dateEnd.value}T00:00:00Z`)
-    ]) 
+        fetch(`https://api.covid19api.com/country/${country.value}?from=${firstDateBeforeDateStart}T00:00:00Z&to=${dateEnd.value}T00:00:00Z`),
+    ])
 
     let summaryData = await summary.value.json()
     let findDataByFilter = await filter.value.json()
-        
+
     let countries = summaryData.Countries
     let countryData = (countries || []).find(element => element.Country == country.value) 
 
-    buildDailyChart(findDataByFilter)
+    buildDailyChart(findDataByFilter, selectFilterValue)
     loadTotalDatas(countryData)
 }
+
 
 let newChart = new Chart(document.getElementById('linhas'), {
     type: 'line',
 })
-function buildDailyChart(findDataByFilter){
+function buildDailyChart(findDataByFilter, selectFilterValue){
+    
+    let dataSearch = selectFilterValue.value
 
     let arrayOfDate = []
-    for (const index in findDataByFilter) {
+    for (let index = 1; index < findDataByFilter.length; index++) {
         let data = formatDate(new Date(findDataByFilter[index].Date).toISOString())
         arrayOfDate.push(data)
     }
 
-    let arrayOfDeaths = []
-    for (const index in findDataByFilter) {
-        arrayOfDeaths.push(findDataByFilter[index].Deaths)
+    let arrayOfData = []
+    for (let index = 1; index < findDataByFilter.length; index++) {
+       arrayOfData.push(dataValue(dataSearch, findDataByFilter[index]) - dataValue(dataSearch, findDataByFilter[index - 1]))
+    }
+
+    let total = 0
+    for (const index in arrayOfData) {
+        total += arrayOfData[index]
+    }
+
+    let average = total/arrayOfData.length
+
+    let arrayOfAverage = []
+    for (let index = 0; index < arrayOfData.length; index++) {
+        arrayOfAverage.push(average)
     }
 
     newChart.data.labels = [...arrayOfDate]
+
     newChart.data.datasets[0] = {
         label: ['Número de mortes'],
-        data: [...arrayOfDeaths],
+        data: [...arrayOfData],
         backgroundColor: 'orange',
         borderColor: 'orange'
     }
 
-    // newChart.data.datasets[1] = {
-    //     label: ['Média de mortes'],
-    //     data: [4,3,2,1,0],
-    //     backgroundColor: 'red',
-    //     borderColor: 'red'
-    // }
+    newChart.data.datasets[1] = {
+        label: ['Média de mortes'],
+        data: [...arrayOfAverage],
+        backgroundColor: 'red',
+        borderColor: 'red'
+    }
+
     newChart.update()
+}
+
+
+function dataValue(dataSearch, findDataByFilter) {
+    return dataSearch == 'Confirmed' ? findDataByFilter.Confirmed 
+    : dataSearch == 'Deaths' ? findDataByFilter.Deaths 
+    : dataSearch == 'Recovered' ? findDataByFilter.Recovered : -1
 }
 
 function populateComboBox(summary) {
@@ -99,9 +125,15 @@ function loadTotalDatas(countryData) {
 }
 
 
-
 function formatDate(date) {   
     let d = date.split('T')
     return d[0]
 }
-  
+
+function getFirstDate(dateStart) {
+    let d = new Date(dateStart);
+    d.setTime(d.getTime() - 1);
+    return (d.getUTCFullYear() + '-' +
+           (d.getUTCMonth() + 1).toString().padStart(2, '0') + '-' +
+            d.getUTCDate().toString().padStart(2, '0'));
+}
