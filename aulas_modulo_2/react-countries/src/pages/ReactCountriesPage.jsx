52c2f37f46c6b05dbaf593ea215a0cf1983fbffd
Boanerges Junior <boanerges_junior@hotmail.com>
2021-06-06 15:48:16 -0300

    useState and TextInput

--- aulas_modulo_2/react-countries/src/pages/ReactCountriesPage.jsx
+++ aulas_modulo_2/react-countries/src/pages/ReactCountriesPage.jsx
@@ -1,12 +1,29 @@
+import { useState } from 'react'
 import Header from '../components/Header'
 import Main from '../components/Main'
 import TextInput from '../components/TextInput'
 
+import { allCountries } from '../data/countries'
+
 export default function ReactCountriesPage() {
-    return (
+
+  const [countryFilter, setContryFilter] = useState('Argentina') 
+
+  function handleCountryFilterChange(newCountryFilter) {
+    setContryFilter(newCountryFilter)
+  }
+
+  return (
       <div>
         <Header>React-coutries</Header> 
-        <Main>Conteudo</Main>
+        <Main>
+          <TextInput
+            id="inputCountrieFilter"
+            labelDescription="Informe o nome do país pelo menos 3 caracteres"
+            inputValue={countryFilter}
+            onInputChange={handleCountryFilterChange}
+            autoFocus/>
+        </Main>
       </div>
     )
 }
