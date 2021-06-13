f34436493799b7ecb4ae54e782e9ec8dcef591e9
Boanerges Junior <boanerges_junior@hotmail.com>
2021-06-13 14:21:00 -0300

    finished app

--- aulas_modulo_2/back-end_to-cards-v2/src/pages/FlashCardsPage.jsx
+++ aulas_modulo_2/back-end_to-cards-v2/src/pages/FlashCardsPage.jsx
@@ -1,93 +1,273 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+
 import Button from '../components/Button';
+import Error from '../components/Error';
 import FlashCard from '../components/FlashCard';
+import FlashCardForm from '../components/FlashCardForm';
+import FlashCardItem from '../components/FlashCardItem';
 import FlashCards from '../components/FlashCards';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
 import Main from '../components/Main';
 import RadioButton from '../components/RadioButton';
-import { allFlashCards } from '../data/allFlashCards';
+
 import { helperShuffleArray } from '../helpers/arrayHelpers';
 
+import {
+  apiCreateFlashCard,
+  apiDeleteFlashCard,
+  apiGetAllFlashCards,
+  apiUpdateFlashCard,
+} from '../services/apiService';
+
+import { getNewId } from '../services/idService';
+
 export default function FlashCardsPage() {
-  const [allCards, setAllCards] = useState(allFlashCards);
+  // Back End
+  const [allCards, setAllCards] = useState([]);
+
+  // Exclusivo para "Estudo"
+  const [studyCards, setStudyCards] = useState([]);
+
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
+  const [createMode, setCreateMode] = useState(true);
+  const [selectedTab, setSelectedTab] = useState(0);
+  const [selectedFlashCard, setSelectedFlashCard] = useState(null);
+
   const [radioButtonShowTitle, setRadioButtonShowTitle] = useState(true);
 
-  function handleButtonClick() {
-    const shuffledCards = helperShuffleArray(allCards);
+  useEffect(() => {
+    // Promise
+    // apiGetAllFlashCards().then(allFlashCards => {
+    //   setAllCards(allFlashCards);
+    // });
 
-    setAllCards(shuffledCards);
+    //IIFE
+    // (async function getAllCards() {
+    //   const backEndAllCards = await apiGetAllFlashCards();
+    //   setAllCards(backEndAllCards);
+    // })();
+
+    async function getAllCards() {
+      try {
+        const backEndAllCards = await apiGetAllFlashCards();
+
+        setAllCards(backEndAllCards);
+
+        setTimeout(() => {
+          setLoading(false);
+        }, 500);
+      } catch (error) {
+        setError(error.message);
+      }
+    }
+
+    getAllCards();
+  }, []);
+
+  function handleShuffle() {
+    const shuffledCards = helperShuffleArray(studyCards);
+
+    setStudyCards(shuffledCards);
   }
+
+  useEffect(() => {
+    setStudyCards(allCards.map(card => ({ ...card, showTitle: true })));
+  }, [allCards]);
 
   function handleRadioShowDescriptionClick() {
     // prettier-ignore
     const updatedCards = 
-      [...allCards].map(card => ({...card, showTitle: false}));
+      [...studyCards].map(card => ({...card, showTitle: false}));
 
-    setAllCards(updatedCards);
+    setStudyCards(updatedCards);
     setRadioButtonShowTitle(false);
   }
 
   function handleRadioShowTitleClick() {
     // prettier-ignore
     const updatedCards = 
-      [...allCards].map(card => ({...card, showTitle: true}));
+      [...studyCards].map(card => ({...card, showTitle: true}));
 
-    setAllCards(updatedCards);
+    setStudyCards(updatedCards);
 
     setRadioButtonShowTitle(true);
   }
 
   function handleToggleFlashCard(cardId) {
-    const updatedCards = [...allCards];
+    const updatedCards = [...studyCards];
     const cardIndex = updatedCards.findIndex(card => card.id === cardId);
     updatedCards[cardIndex].showTitle = !updatedCards[cardIndex].showTitle;
 
-    setAllCards(updatedCards);
+    setStudyCards(updatedCards);
+  }
+
+  async function handleDeleteFlashCard(cardId) {
+    try {
+      // Back End
+      await apiDeleteFlashCard(cardId);
+
+      // Front End
+      setAllCards(allCards.filter(card => card.id !== cardId));
+
+      setError('');
+    } catch (error) {
+      setError(error.message);
+    }
+  }
+
+  function handleEditFlashCard(card) {
+    setCreateMode(false);
+    setSelectedTab(1);
+    setSelectedFlashCard(card);
+  }
+
+  function handleNewFlashCard() {
+    setCreateMode(true);
+    setSelectedFlashCard(null);
+  }
+
+  function handleTabSelect(tabIndex) {
+    setSelectedTab(tabIndex);
+  }
+
+  async function handlePersist(title, description) {
+    if (createMode) {
+      try {
+        // Back End
+        const newFlashCard = await apiCreateFlashCard(title, description);
+
+        // Front End
+        setAllCards([...allCards, newFlashCard]);
+
+        setError('');
+      } catch (error) {
+        setError(error.message);
+      }
+    } else {
+      try {
+        // Back End
+        await apiUpdateFlashCard(selectedFlashCard.id, title, description);
+
+        // Front End
+        setAllCards(
+          allCards.map(card => {
+            if (card.id === selectedFlashCard.id) {
+              return { ...card, title, description };
+            }
+            return card;
+          })
+        );
+
+        setSelectedFlashCard(null);
+        setCreateMode(true);
+        setError('');
+      } catch (error) {
+        setError(error.message);
+      }
+    }
+  }
+
+  let mainJsx = (
+    <div className="flex justify-center my-4">
+      <Loading />
+    </div>
+  );
+
+  if (error) {
+    mainJsx = <Error>{error}</Error>;
+  }
+
+  if (!loading && !error) {
+    mainJsx = (
+      <>
+        <Tabs selectedIndex={selectedTab} onSelect={handleTabSelect}>
+          <TabList>
+            <Tab>Listagem</Tab>
+            <Tab>Cadastro</Tab>
+            <Tab>Estudo</Tab>
+          </TabList>
+
+          <TabPanel>
+            {allCards.map(flashCard => {
+              return (
+                <FlashCardItem
+                  key={flashCard.id}
+                  onDelete={handleDeleteFlashCard}
+                  onEdit={handleEditFlashCard}
+                >
+                  {flashCard}
+                </FlashCardItem>
+              );
+            })}
+          </TabPanel>
+
+          <TabPanel>
+            <div className="my-4">
+              <Button onButtonClick={handleNewFlashCard}>
+                Novo flash card
+              </Button>
+            </div>
+
+            <FlashCardForm createMode={createMode} onPersist={handlePersist}>
+              {selectedFlashCard}
+            </FlashCardForm>
+          </TabPanel>
+
+          <TabPanel>
+            <div className="text-center mb-4">
+              <Button onButtonClick={handleShuffle}>Embaralhar cards</Button>
+            </div>
+
+            <div className="flex flex-row items-center justify-center space-x-4 m-4">
+              <RadioButton
+                id="radioButtonShowTitle"
+                name="showInfo"
+                buttonChecked={radioButtonShowTitle}
+                onButtonClick={handleRadioShowTitleClick}
+              >
+                Mostrar título
+              </RadioButton>
+
+              <RadioButton
+                id="radioButtonShowDescription"
+                name="showInfo"
+                buttonChecked={!radioButtonShowTitle}
+                onButtonClick={handleRadioShowDescriptionClick}
+              >
+                Mostrar descrição
+              </RadioButton>
+            </div>
+
+            <FlashCards>
+              {studyCards.map(({ id, title, description, showTitle }) => {
+                return (
+                  <FlashCard
+                    key={id}
+                    id={id}
+                    title={title}
+                    description={description}
+                    showFlashCardTitle={showTitle}
+                    onToggleFlashCard={handleToggleFlashCard}
+                  />
+                );
+              })}
+            </FlashCards>
+          </TabPanel>
+        </Tabs>
+      </>
+    );
   }
 
   return (
     <>
-      <Header>react-flash-cards-v1</Header>
-      <Main>
-        <div className="text-center mb-4">
-          <Button onButtonClick={handleButtonClick}>Embaralhar cards</Button>
-        </div>
+      <Header>react-flash-cards-v2</Header>
 
-        <div className="flex flex-row items-center justify-center space-x-4 m-4">
-          <RadioButton
-            id="radioButtonShowTitle"
-            name="showInfo"
-            buttonChecked={radioButtonShowTitle}
-            onButtonClick={handleRadioShowTitleClick}
-          >
-            Mostrar título
-          </RadioButton>
-
-          <RadioButton
-            id="radioButtonShowDescription"
-            name="showInfo"
-            buttonChecked={!radioButtonShowTitle}
-            onButtonClick={handleRadioShowDescriptionClick}
-          >
-            Mostrar descrição
-          </RadioButton>
-        </div>
-
-        <FlashCards>
-          {allCards.map(({ id, title, description, showTitle }) => {
-            return (
-              <FlashCard
-                key={id}
-                id={id}
-                title={title}
-                description={description}
-                showFlashCardTitle={showTitle}
-                onToggleFlashCard={handleToggleFlashCard}
-              />
-            );
-          })}
-        </FlashCards>
-      </Main>
+      <Main>{mainJsx}</Main>
     </>
   );
 }
