752db61a79246b1f45b9a883cb6e3eed34fcbb3c
Boanerges Junior <boanerges_junior@hotmail.com>
2021-06-15 14:59:04 -0300

    CardInfo created

--- react-elections/frontend/src/components/CardCandidate.jsx
+++ react-elections/frontend/src/components/CardCandidate.jsx
@@ -1,19 +1,14 @@
-import antman from '../assets/antman.png'
+import CardInfo from './CardInfo'
 
-export default function CardCandidate() {
+export default function CardCandidate({
+    arrayOfAllCandidates = [],
+    infoElection = []
+}) {
     return (
-        <div className="flex justify-center font-mono">
-            <div className="shadow-xl w-48 h-44 rounded-sm m-2">
-                <div className="flex flex-row items-center space-x-9">
-                    <img src={antman} alt="antman" className="w-14 rounded-full m-2"/>
-                    <div className="flex flex-col items-center">
-                        <span>14%</span>
-                        <span className="text-xs">12312 votos</span>
-                    </div>
-                </div>
-                <div className="text-xl text-center m-5">name</div>
-                <div className="font-sans text-center">Não eleito</div>
-            </div>
+        <div className="flex flex-row flex-wrap justify-center font-mono">
+           
+            return <CardInfo name={arrayOfAllCandidates.name}/>
+
         </div>
     )
 }
--- react-elections/frontend/src/pages/ElectionsInfo.jsx
+++ react-elections/frontend/src/pages/ElectionsInfo.jsx
@@ -1,4 +1,6 @@
 import { apiGetAllCities } from '../services/citiesServices/apiService'
+import { apiGetAllCandidates } from '../services/candidatesSevices/apiService'
+import { apiGetAllElection } from '../services/electionServices/apiService'
 import { useEffect, useState } from 'react';
 import _ from 'lodash'
 
@@ -12,6 +14,9 @@ export default function ElectionsInfo() {
 
     const [allCities, setAllCities] = useState([])
     const [selectedCity, setSelectedCity] = useState('Asgard')
+    const [allCandidates, setAllCandidates] = useState([])
+    const [allElection, setAllElection] = useState([])
+    const [electionFilteredByCity, setElectionFilteredByCity] = useState([])
 
     useEffect(() => {
         async function getAllCitiesOrdered() {
@@ -27,9 +32,36 @@ export default function ElectionsInfo() {
         getAllCitiesOrdered()
     }, [])
 
+    useEffect(() => {
+        async function getAllCandidates() {
+            const candidates = await apiGetAllCandidates()
+
+            setAllCandidates(candidates)
+        }
+
+        getAllCandidates()
+    }, [])
+
+    useEffect(() => {
+        async function getAllElections() {
+            const election = await apiGetAllElection()
+
+            setAllElection(election)
+        }
+
+        getAllElections()
+    }, [])
+
     function handleChangeSelected(cityName) {
         setSelectedCity(cityName)
     }
+
+    useEffect(() => {
+        const cityName = selectedCity.length === 0 ? 'Asgard' : selectedCity
+        const city = _.filter(allCities, {name: cityName})
+        const electionFiltered = _.filter(allElection, {cityId: city[0].id})  
+        setElectionFilteredByCity(electionFiltered)
+    }, [selectedCity, allCities, allCandidates])
 
     return (
         <>
@@ -40,10 +72,11 @@ export default function ElectionsInfo() {
                 {allCities.length !== 0 &&            
                     <Title arrayOfAllCities={allCities} comboSelectedCity={selectedCity}>Eleições em {selectedCity}</Title>
                 }
-                <div className="flex flex-row flex-wrap  justify-center max-w-5xl">
-                    <CardCandidate />
-                    <CardCandidate />
-                    <CardCandidate />
+
+                <div className="flex justify-center max-w-5xl">                   
+                    {electionFilteredByCity.map(candidates => { 
+                        return <CardCandidate key={candidates.name} arrayOfAllCandidates={allCandidates} infoElection={electionFilteredByCity}/>
+                    })}
                 </div>
                     
             </Main>
