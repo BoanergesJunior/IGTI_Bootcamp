ed06bf8371de79b43080d8d69a9c791958692f50
Boanerges Junior <boanerges_junior@hotmail.com>
2021-06-22 14:04:55 -0300

    Calendar component created

--- aulas_modulo_2/agenda-typescript/frontend/src/app/CalendarScreen.tsx
+++ aulas_modulo_2/agenda-typescript/frontend/src/app/CalendarScreen.tsx
@@ -1,62 +1,17 @@
-import { makeStyles } from '@material-ui/core';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
-import Icon from '@material-ui/core/Icon';
+
 import { getCalendarsEndpoint, getEventEndpoint, ICalendar, IEvent } from './backend';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import { CalendarsView } from './CalendarsView'
 import { CalendarHeader } from './CalendarHeader'
-
-const DAYS_OF_WEEK = ["DOM", "SEG", "TER", "QUA", "QUI", "SEX", "SAB"]
-
-const useStyles = makeStyles({
-    table: {
-        borderTop: "1px solid rgb(224, 224, 224)",
-        minHeight: "100%",
-        tableLayout: "fixed",
-        "& td ~ td, & th ~ th": {
-            borderLeft: "1px solid rgb(224, 224, 224)"
-        },
-        "& td": {
-            verticalAlign: "top",
-            overflow: "hidden" ,
-            padding: "8px 4px"
-        }
-    },
-    dayOfMonth: {
-        fontWeight: 500,
-        marginBottom: "4px"
-    },
-    event: {
-        display: "flex",
-        alignItems: "center",
-        background:"none",
-        border: "none",
-        cursor: "pointer",
-        textAlign: "left",
-        whiteSpace: "nowrap",
-        margin: "4px 0px"
-    },
-    eventBackground: {
-        display: "inline-block",
-        color: "white",
-        padding: "2px 4px",
-        borderRadius: "4px"
-    }
-  });
+import { Calendar, ICalendarCell, IEventWithCalendar } from './Calendar'
 
 export default function CalendarScreen() {
 
     const { month } = useParams<{month: string}>()
 
-    const classes = useStyles()
     const [events, setEvents] = useState<IEvent[]>([])
     const [calendars, setCalendars] = useState<ICalendar[]>([])
     const [calendarsSelected, setCalendarsSelected] = useState<boolean[]>([])
@@ -103,61 +58,10 @@ export default function CalendarScreen() {
             <Box flex="1" display="flex" flexDirection="column">
                 <CalendarHeader month={month} />
 
-                <TableContainer style={{flex: "1"}} component={"div"}>
-                    <Table className={classes.table} aria-label="simple table">
-                        <TableHead>
-                            <TableRow>
-                                {DAYS_OF_WEEK.map(day => 
-                                <TableCell key={day} align="center">
-                                    {day}
-                                </TableCell>)}
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {weeks.map((week, i) => (
-                                <TableRow key={i}>
-                                    {week.map(cell => 
-                                        <TableCell key={cell.date} align="center">
-                                            <div className={classes.dayOfMonth}>{cell.dayOfMonth}</div>
-
-                                            {cell.events.map(event => {
-                                                
-                                                const color = event.calendar.color
-                                                
-                                                return (
-                                                    <button key={event.id} className={classes.event}>
-                                                        {event.time && 
-                                                            <>
-                                                                <Icon style={{color}} fontSize="inherit">
-                                                                    watch_later
-                                                                </Icon>
-                                                                <Box component="span" margin="0 4px">
-                                                                    {event.time}
-                                                                </Box>
-                                                            </>
-                                                        }
-                                                        {event.time ? <span>{event.desc}</span> 
-                                                        : <span className={classes.eventBackground} style={{backgroundColor: color}}>{event.desc}</span>}
-                                                    </button>
-                                                )}
-                                                )}
-                                        </TableCell>)}
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
+                <Calendar weeks={weeks}/>
             </Box>
         </Box>        
     )
-          
-    type IEventWithCalendar = IEvent & {calendar: ICalendar}
-
-    interface ICalendarCell{
-        date: string
-        dayOfMonth: number
-        events: IEventWithCalendar[]
-    }
 
     function generateCalendar(
         date: string, 
@@ -179,7 +83,7 @@ export default function CalendarScreen() {
         do {
             const week: ICalendarCell[] = []
 
-            for (let i = 0; i < DAYS_OF_WEEK.length; i++) {
+            for (let i = 0; i < 7; i++) {
                 const isoDate = `${currentDay.getFullYear()}-${(currentDay.getMonth() + 1)
                                     .toString().padStart(2, "0")}-${currentDay.getDate()
                                     .toString().padStart(2, "0")}`
@@ -206,7 +110,5 @@ export default function CalendarScreen() {
 
         return weeks
     }
-
-  
 }
 
