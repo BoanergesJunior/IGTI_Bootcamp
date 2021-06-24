52c40ff52f17e7d96c13473c0d4b7e65be9c4d4d
Boanerges Junior <boanerges_junior@hotmail.com>
2021-06-24 18:37:30 -0300

    LoginScreen component created

--- aulas_modulo_2/agenda-typescript/frontend/src/app/App.tsx
+++ aulas_modulo_2/agenda-typescript/frontend/src/app/App.tsx
@@ -1,14 +1,22 @@
 import CalendarScreen from "./CalendarScreen";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect
-} from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route, Redirect} from "react-router-dom";
 import { getToday } from "./dateFunctions";
+import { useEffect, useState } from "react";
+import { getUserEndpoint, IUser } from "./backend";
+import LoginScreen from './LoginScreen'
 
 function App() {
-   const month = getToday().substring(0,7)
+  const month = getToday().substring(0,7)
+
+  const [user, setUser] = useState<IUser | null>(null)
+
+  useEffect(() => {
+    getUserEndpoint().then(setUser, 
+      () => setUser(null)
+    )
+  }, [])
+
+  if(user) {
     return (
       <Router>
         <Switch>
@@ -19,6 +27,9 @@ function App() {
         </Switch>
       </Router>
     )
+  } else {
+    return <LoginScreen onSignIn={setUser}/>
+  }
 }
 
 export default App;
--- aulas_modulo_2/agenda-typescript/frontend/src/app/backend.tsx
+++ aulas_modulo_2/agenda-typescript/frontend/src/app/backend.tsx
@@ -16,53 +16,81 @@ export interface IEditingEvent {
     calendarId: number
 }
 
+export interface IUser{
+    name: string
+    email: string
+}
 
 export function getCalendarsEndpoint(): Promise<ICalendar[]> {
-    return fetch("http://localhost:8080/calendars")
-    .then(resp => {
-        return resp.json()
+    return fetch("http://localhost:8080/calendars", {
+        credentials: "include",
     })
+    .then(handleResponse)
 }
 
 export function getEventEndpoint(from: string, to: string): Promise<IEvent[]> {
-    return fetch(`http://localhost:8080/events?date_gte=${from}&date_lte=${to}&_sort=date,time`)
-    .then(resp => {
-        return resp.json()
+    return fetch(`http://localhost:8080/events?date_gte=${from}&date_lte=${to}&_sort=date,time`, {
+        credentials: "include",
     })
+    .then(handleResponse)
 }
 
 
 export function createEventEndpoint(event: IEditingEvent): Promise<IEvent> {
     return fetch(`http://localhost:8080/events`, {
+        credentials: "include",    
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify(event)  
     })
-    .then(resp => {
-        return resp.json()
-    })
+    .then(handleResponse)
 }
 
 export function updateEventEndpoint(event: IEditingEvent): Promise<IEvent> {
     return fetch(`http://localhost:8080/events${event.id}`, {
+        credentials: "include",    
         method: "PUT",
         headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify(event)  
     })
-    .then(resp => {
-        return resp.json()
-    })
+    .then(handleResponse)
 }
 
 export function deleteEventEndpoint(eventId: number): Promise<void> {
     return fetch(`http://localhost:8080/events/${eventId}`, {
+        credentials: "include",  
         method: "DELETE", 
     })
-    .then(resp => {
-        return resp.json()
+    .then(handleResponse)
+}
+
+export function getUserEndpoint(): Promise<IUser> {
+    return fetch(`http://localhost:8080/auth/user`, {
+        credentials: "include"
     })
+    .then(handleResponse)
+}
+
+export function singInEndpoint(email: string, password: string): Promise<IUser> {
+    return fetch(`http://localhost:8080/auth/login`, {
+    credentials: "include",    
+    method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({email, password})
+    })
+    .then(handleResponse)
+}
+
+function handleResponse(resp: Response) {
+    if(resp.ok) {
+        return resp.json()
+    } else {
+        throw new Error(resp.statusText)
+    }
 }
