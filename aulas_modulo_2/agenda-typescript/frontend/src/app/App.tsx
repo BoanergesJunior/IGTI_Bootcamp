66688ce427492ed55d84c00f62fdf2701f3cc95b
Boanerges Junior <boanerges_junior@hotmail.com>
2021-06-27 14:31:07 -0300

    last modifications

--- aulas_modulo_2/agenda-typescript/frontend/src/app/App.tsx
+++ aulas_modulo_2/agenda-typescript/frontend/src/app/App.tsx
@@ -1,7 +1,7 @@
 import CalendarScreen from "./CalendarScreen";
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import { getToday } from "./dateFunctions";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { getUserEndpoint, IUser } from "./backend";
 import LoginScreen from "./LoginScreen";
 import { authContext } from "./authContext";
@@ -33,6 +33,50 @@ function App() {
     );
   } else {
     return <LoginScreen onSignIn={setUser} />;
+  }
+}
+
+class APP2 extends React.Component<{}, { user: IUser | null }> {
+  setUser: (user: IUser) => void;
+  onSignOut: () => void;
+
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      user: null,
+    };
+    this.setUser = (user: IUser) => {
+      this.setState({ user });
+    };
+
+    this.onSignOut = () => {
+      this.setState({ user: null });
+    };
+  }
+
+  render() {
+    const month = getToday().substring(0, 7);
+
+    const { user } = this.state;
+    if (user) {
+      return (
+        <authContext.Provider value={{ user, onSignOut: this.onSignOut }}>
+          <Router>
+            <Switch>
+              <Route path="/calendar/:month">
+                <CalendarScreen />
+              </Route>
+              <Redirect to={{ pathname: "/calendar/" + month }} />
+            </Switch>
+          </Router>
+        </authContext.Provider>
+      );
+    } else {
+      return <LoginScreen onSignIn={this.setUser} />;
+    }
+  }
+  componentDidMount() {
+    getUserEndpoint().then(this.setUser, this.onSignOut);
   }
 }
 
